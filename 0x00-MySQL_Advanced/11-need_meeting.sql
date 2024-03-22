6383403195943c8c80ca39a806d49bb9e3a94342
2oothpick <kobbyboateng14@gmail.com>
2024-03-22 05:37:21 +0000

    Add 11-need_meeting.sql

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,9 @@
+-- Task 11-need_meeting.sql
+
+DROP VIEW IF EXISTS need_meeting;
+
+CREATE VIEW need_meeting AS
+SELECT name 
+FROM students 
+WHERE score < 80 AND 
+(last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
